38a8a11af81ec8cb586d0eef1c38f7722da37128
Ali Shaikh <ali.shaikh@hotmail.com>
2021-02-26 15:46:55 -0800

    Lab1

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,15 +11,46 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
+    @IBOutlet var btnOptionOne: UIButton!
+    @IBOutlet var btnOptionTwo: UIButton!
+    @IBOutlet var btnOptionThree: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Rounded Corners to Question (Label)
+        card.layer.cornerRadius = 20.0
+        card.clipsToBounds = true
+        
+        //Shadow to Question
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        
+        //Rounded Corners on answer buttons
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        
+
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        if(frontLabel.isHidden == true){
+            frontLabel.isHidden = false
+        }
+        else{
+            frontLabel.isHidden = true
+        }
     }
     
+    
+    
 }
-
